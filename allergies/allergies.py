6db01d8f2650891a8435412e0e884e03eebde1e4
mRcfps <1402491442@qq.com>
2018-01-27 12:44:25 +0800

    allergies: iteration 2, bitwise tricks

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,36 +1,22 @@
-import math
-
-ALLERGIES = [
-    'eggs', 'peanuts', 'shellfish', 'strawberries',
-    'tomatoes', 'chocolate', 'pollen', 'cats',
-]
-
+allergies = {
+    1: "eggs",
+    2: "peanuts",
+    4: "shellfish",
+    8: "strawberries",
+    16: "tomatoes",
+    32: "chocolate",
+    64: "pollen",
+    128: "cats",
+}
 
 class Allergies(object):
 
     def __init__(self, score):
         self.score = score
-        self.allergies = []
 
     def is_allergic_to(self, item):
         return item in self.lst
 
     @property
     def lst(self):
-        if self.score == 0:
-            return self.allergies
-
-        # 2^power <= score < 2^(power+1)
-        power = math.floor(math.log2(self.score))
-
-        # Get a copy of score to avert side effect
-        remain = self.score
-
-        while remain > 0 and power >= 0:
-            if remain >= 2**power:
-                if power < len(ALLERGIES):
-                    self.allergies.append(ALLERGIES[power])
-                remain -= 2**power
-            power -= 1
-
-        return self.allergies
+        return [item for i, item in allergies.items() if self.score & i]
