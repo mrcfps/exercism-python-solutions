c11d90c11aae3fe8fe23d257cf9ed76b05a65cdb
mRcfps <1402491442@qq.com>
2018-01-29 22:32:50 +0800

    bowling: iteration 2

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -5,12 +5,10 @@ class Frame(object):
     def __init__(self, idx):
         self.idx = idx
         self.throws = []
-        self.fill_balls = []
-        self.next_frame = None
-        self.frame_after_next = None
 
     @property
     def total_pins(self):
+        """Total pins knocked down in a frame."""
         return sum(self.throws)
 
     def is_strike(self):
@@ -23,103 +21,79 @@ class Frame(object):
         return len(self.throws) == 2 and self.total_pins < 10
 
     def is_closed(self):
-        """Return if a frame is over."""
-        if self.total_pins < 10:
-            return len(self.throws) == 2
-        elif self.idx < MAX_FRAME - 1:
-            # A strike or spare not in the last
-            return True
-        elif self.is_spare():  # Tenth frame that is spare
-            return len(self.fill_balls) == 1
-        elif self.is_strike():  # Tenth frame that is strike
-            return len(self.fill_balls) == 2
-
-    def throw_bonus(self, pins):
-        """Throw a bonus in the tenth frame."""
-        self.fill_balls.append(pins)
-
-        # Check against invalid fill balls, e.g. [3, 10]
-        if (len(self.fill_balls) == 2 and
-            self.fill_balls[0] != 10 and
-            sum(self.fill_balls) > 10):
-            raise ValueError("invalid fill balls")
-
-        # Check if there are more bonuses than it should be
-        if self.is_strike() and len(self.fill_balls) > 2:
-            raise ValueError(
-                "wrong number of fill balls when the tenth frame is a strike")
-        elif self.is_spare() and len(self.fill_balls) > 1:
-            raise ValueError(
-                "wrong number of fill balls when the tenth frame is a spare")
+        """Return whether a frame is over."""
+        return self.total_pins == 10 or len(self.throws) == 2
 
     def throw(self, pins):
-        if not 0 <= pins <= 10:
-            raise ValueError("invalid pins")
+        if self.total_pins + pins > 10:
+            raise ValueError("a frame's rolls cannot exceed 10")
+        self.throws.append(pins)
 
-        if self.idx == MAX_FRAME - 1:
-            if self.is_closed():
-                raise IndexError(
-                    "cannot throw bonus with a open tenth frame")
-            if self.is_strike() or self.is_spare():
-                self.throw_bonus(pins)
-            else:
-                self.throws.append(pins)
-        else:
-            self.throws.append(pins)
-            if self.total_pins > 10:
-                raise ValueError("cannot roll more than 10 pins in a frame")
-
-    def score(self):
-        if self.is_open():
-            score = self.total_pins
+    def score(self, next_throws):
+        result = self.total_pins
+        if self.is_strike():
+            result += sum(next_throws[:2])
         elif self.is_spare():
-            if self.next_frame is not None:
-                score = self.total_pins + self.next_frame.throws[0]
-            else:
-                # The spare is the tenth frame
-                score = self.total_pins + self.fill_balls[0]
-        else:  # It's a strike
-            if self.next_frame is None:
-                # The strike is the tenth frame
-                score = 10 + sum(self.fill_balls)
-            elif not self.next_frame.is_strike():
-                # A not-the-last strike, followed by a non-strike
-                score = 10 + self.next_frame.total_pins
-            elif self.frame_after_next is None:
-                # The strike is the ninth frame, followed by a strike
-                score = 20 + self.next_frame.fill_balls[0]
-            else:
-                # The strike is a frame with idx < 8, followed by a strike
-                score = 20 + self.frame_after_next.throws[0]
-
-        return score
+            result += sum(next_throws[:1])
+        return result
 
 
 class BowlingGame(object):
     def __init__(self):
         self.current_frame_idx = 0
+        self.bonus_throws = []
         self.frames = [Frame(idx) for idx in range(MAX_FRAME)]
-
-        # Init next_frame and frame_after_next attributes in each frame
-        for idx, frame in enumerate(self.frames):
-            frame.next_frame = self.frames[idx+1] if idx < MAX_FRAME - 1 else None
-            frame.frame_after_next = self.frames[idx+2] if idx < MAX_FRAME - 2 else None
 
     @property
     def current_frame(self):
         return self.frames[self.current_frame_idx]
 
-    def roll(self, pins):
-        if self.current_frame.is_closed():
-            self.current_frame_idx += 1
+    def next_throws(self, frame_idx):
+        """Return a frame's next throws in the form of a list."""
+        throws = []
+        for idx in range(frame_idx + 1, MAX_FRAME):
+            throws.extend(self.frames[idx].throws)
+        throws.extend(self.bonus_throws)
+        return throws
 
-        self.current_frame.throw(pins)
+    def roll_bonus(self, pins):
+        tenth_frame = self.frames[-1]
+        if tenth_frame.is_open():
+            raise IndexError("cannot throw bonus with an open tenth frame")
+
+        self.bonus_throws.append(pins)
+
+        # Check against invalid fill balls, e.g. [3, 10]
+        if (len(self.bonus_throws) == 2 and self.bonus_throws[0] != 10 and
+                sum(self.bonus_throws) > 10):
+            raise ValueError("invalid fill balls")
+
+        # Check if there are more bonuses than it should be
+        if tenth_frame.is_strike() and len(self.bonus_throws) > 2:
+            raise ValueError(
+                "wrong number of fill balls when the tenth frame is a strike")
+        elif tenth_frame.is_spare() and len(self.bonus_throws) > 1:
+            raise ValueError(
+                "wrong number of fill balls when the tenth frame is a spare")
+
+    def roll(self, pins):
+        if not 0 <= pins <= 10:
+            raise ValueError("invalid pins")
+        elif self.current_frame_idx == MAX_FRAME:
+            self.roll_bonus(pins)
+        else:
+            self.current_frame.throw(pins)
+            if self.current_frame.is_closed():
+                self.current_frame_idx += 1
 
     def score(self):
-        if self.current_frame_idx < MAX_FRAME - 1:
+        if self.current_frame_idx < MAX_FRAME:
             raise IndexError("frame less than 10")
-
-        if not self.frames[-1].is_closed():
-            raise IndexError("bonus must be rolled")
-
-        return sum(frame.score() for frame in self.frames)
+        if self.frames[-1].is_spare() and len(self.bonus_throws) != 1:
+            raise IndexError(
+                "one bonus must be rolled when the tenth frame is spare")
+        if self.frames[-1].is_strike() and len(self.bonus_throws) != 2:
+            raise IndexError(
+                "two bonuses must be rolled when the tenth frame is strike")
+        return sum(frame.score(self.next_throws(frame.idx))
+                   for frame in self.frames)
