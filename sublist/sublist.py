b1541c8bf76d04a4e9bc1a46a4e111afaaf2a4ba
mRcfps <1402491442@qq.com>
2018-02-08 11:55:53 +0800

    Implement exercise sublist

---
+++ sublist/sublist.py
@@ -0,0 +1,20 @@
+SUBLIST = 1
+SUPERLIST = 2
+EQUAL = 3
+UNEQUAL = 4
+
+
+def check_lists(first_list, second_list):
+    if first_list == second_list:
+        return EQUAL
+
+    # Convert lists to comma-separated strings
+    first = ','.join(str(c) for c in first_list)
+    second = ','.join(str(c) for c in second_list)
+
+    if first in second:
+        return SUBLIST
+    elif second in first:
+        return SUPERLIST
+    else:
+        return UNEQUAL
